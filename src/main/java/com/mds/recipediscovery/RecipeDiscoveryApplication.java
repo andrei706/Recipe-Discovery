674fac01bb0759f3ecd8959bc738cafae90a10c0
andrei706 <andrei12342526@gmail.com>
2026-04-16 21:23:28 +0300

    Initial commit

---
+++ src/main/java/com/mds/recipediscovery/RecipeDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.mds.recipediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mds/recipediscovery/RecipeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mds.recipediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
